e9d4e301cc5e027dfb96322f378149b2a78abcbe
bhadreshdesai <bhadresh_desai@hotmail.com>
2020-12-31 23:10:55 +0000

    Fixes #1 Create project

---
+++ src/test/java/bdd/demo/appjava/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package bdd.demo.appjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
