5775f7feda9059ff8aabe6f7cbf4b73f690bb977
Bhadresh Desai <bhadresh_desai@hotmail.com>
2021-04-28 21:17:21 +0100

    Add open-api docs

--- src/main/java/bdd/demo/appjava/employee/Constants.java
+++ src/main/java/bdd/demo/appjava/employee/Constants.java
@@ -2,5 +2,6 @@ package bdd.demo.appjava.employee;
 
 public class Constants {
     public static final String APIPATH_EMPLOYEES = "/api/employees";
+    public static final String DESC_EMPLOYEES_API = "The employee CRUD API";
     public static final String TAG_EMPLOYEES = "Employees";
 }
--- src/main/java/bdd/demo/appjava/employee/EmployeeController.java
+++ src/main/java/bdd/demo/appjava/employee/EmployeeController.java
@@ -1,5 +1,9 @@
 package bdd.demo.appjava.employee;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +15,10 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.net.URI;
 import java.util.Optional;
 
-import static bdd.demo.appjava.employee.Constants.APIPATH_EMPLOYEES;
-import static bdd.demo.appjava.employee.Constants.TAG_EMPLOYEES;
+import static bdd.demo.appjava.employee.Constants.*;
 
 // https://github.com/dariawantech/spring-boot-rest-springdoc-openapi/blob/master/src/main/java/com/dariawan/contactapp/controller/ContactController.java
-@Tag(name = TAG_EMPLOYEES)
+@Tag(name = TAG_EMPLOYEES, description = DESC_EMPLOYEES_API)
 @RestController
 @RequestMapping(path = APIPATH_EMPLOYEES)
 @Slf4j
@@ -36,6 +39,11 @@ public class EmployeeController {
                 ;
     }
 
+    @Operation(summary = "Get employee by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation"),
+            @ApiResponse(responseCode = "400", description = "Invalid username supplied", content = @Content),
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content) })
     @GetMapping(path = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<Employee> getById(@PathVariable("id") Long id) {
         Optional<Employee> employee = employeeService.getById(id);
